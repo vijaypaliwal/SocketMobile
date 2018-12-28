89192b40541cdead7fa2748bd375b45e6a5edb4c
vijayp <vijay.paliwal@hotmail.com>
2018-12-28 10:40:04 +0530

    this is just fortest

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com-socketmobile-scanapi-cordova.ScanApiCordova",
+    "file": "plugins/com-socketmobile-scanapi-cordova/www/ScanApiCordova.js",
+    "pluginId": "com-socketmobile-scanapi-cordova",
+    "clobbers": [
+      "SocketScanApi"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "com-socketmobile-scanapi-cordova": "0.1.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ www/js/index.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        SocketScanApi.useScanApi('', scanApiNotification);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    scanApiNotification:function(event)
+    {
+        try {
+            event = JSON.parse(event);
+            if (event.type) {
+                alert('receive an event: '+ event.type);
+                document.getElementById('eventRec')[0].innerHTML='receive an event: '+event.type; ;
+               
+                if (event.type === 'decodedData') {
+                    document.getElementById('eventData')[0].innerHTML = event.decodedData.join(" and ");;
+                   // let decodedData = event.decodedData.map(c => String.fromCharCode(c)).join('');
+                    alert('decodedData: ', decodedData);
+                }
+            }
+        } catch(e) {
+            alert('event received: ', event);
+        }
+    }
+};
+
+app.initialize();
